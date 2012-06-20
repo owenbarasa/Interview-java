d2e5754c7b87ec4c48f071171be761c2352b2227
Wenqi Zhu <7.cheer@gmail.com>
2012-06-19 22:16:23 -0700

    BST

--- src/Tree/BST.java
+++ src/Tree/BST.java
@@ -2,6 +2,7 @@
  * 
  */
 package Tree;
+import java.util.*;
 
 
 /**
@@ -13,15 +14,17 @@ package Tree;
 public class BST {
 	private Node root;
 	private int size;
-	 Node current = root;
+	 //Node current = root;
 	public BST(){
 		root = null;
 		size = 0;
 	}
-	
+	public Node getRoot(){
+		return root;
+	}
 	public Node create(int x){
-		Node newNode = new Node(x);
-		return newNode;
+		Node node = new Node(x);
+		return node;
 	}
 	public boolean isEmpty(){
 		return root == null ? true:false;
@@ -32,23 +35,32 @@ public class BST {
 	public int getSize(Node node){
 		return root == null ? 0 : (getSize(node.getLeft())+1+getSize(node.getRight()));
 	}
-	public Node insert(Node newNode,int data){
-		if(newNode == null){
-			newNode = create(data);
-			return newNode;
+	
+	public void insert(int data){
+		Node current = insert(root,data);
+		if(isEmpty()==true && current !=null){
+			root = current;
 		}
-		else if (newNode.compareTo(data)<0){
-			if (newNode.getRight() == null) 
-				newNode.setRight(create(data));
+		size++;
+		
+	}
+	private Node insert(Node node,int data){
+		if(node == null){
+			node = create(data);
+			return node;
+		}
+		else if (node.compareTo(data)<0){
+			if (node.getRight() == null) 
+				node.setRight(create(data));
 			else{
-				newNode = insert(newNode.getRight(),data);
+				node = insert(node.getRight(),data);
 			}
 		}
-		else if (newNode.compareTo(data)>=0){
-			if(newNode.getLeft()==null)
-				newNode.setLeft(create(data));
+		else if (node.compareTo(data)>=0){
+			if(node.getLeft()==null)
+				node.setLeft(create(data));
 			else{
-				newNode = insert(newNode.getLeft(),data);
+				node = insert(node.getLeft(),data);
 			}
 		}
 		return null;
@@ -59,7 +71,7 @@ public class BST {
 			return false;
 		}
 		else{
-			current = find(root, data);
+			Node current = find(root, data);
 			if (current!= null) return true;
 			else return false;
 		}
@@ -79,7 +91,7 @@ public class BST {
     public boolean deleteNorecursion(int data){
     	if(isEmpty()) return false;
     	else {
-    			Node parent =root;
+    			Node current=root,parent =root;
     			boolean found = false;
     			while(current != null && !found){
     				if(current.compareTo(data) == 0){
@@ -118,7 +130,7 @@ public class BST {
 
 	private Node replacement(Node node){// return the node that will replace in the deleted one
 		Node result = null;
-		Node parent = node;
+		Node current=node, parent = node;
 		
 		if (node.getLeft() == null && node.getRight() == null){//node to be deleted is a leaf node
 			return null;
@@ -133,7 +145,7 @@ public class BST {
 			  find the leftmost child in its right subtree.
 			*/
 			current = node.getRight();
-		}
+		
 		    while(current.getLeft()!=null){
 			parent = current;
 			current = current.getLeft();
@@ -147,6 +159,7 @@ public class BST {
 			   current.setRight(node.getRight());
 		   }
 		   result = current;
+		}
 		   return result;	   
 	       }
 
@@ -244,6 +257,138 @@ public class BST {
 		System.out.print(node.getData() + " ");
 	}
 	
+	public void preorderTraversalIterative(Node node){
+
+		Stack<Node> stack = new Stack<Node>();
+		stack.push(node);
+		while(!stack.empty()){
+			Node current = stack.pop();
+			if(current == null) break;
+			System.out.print(current.getData() + " ");
+			Node n = current.getRight();
+			if(n!=null)
+				stack.push(n);
+			n = current.getLeft();
+			if(n!=null)
+				stack.push(n);
+		}
+	}
 	
+	public void inorderTraversalIterative(Node node){
+//		1) Create an empty stack.
+//		2) Initialize current node as root
+//		3) Push the current node to stack and set current = current->left until current is NULL
+//		4) If current is NULL and stack is not empty then
+//		     a) Pop the top item from stack.
+//		     b) Print the popped item, set current = current->right
+//		     c) Go to step 3.
+//		5) If current is NULL and stack is empty then we are done.
+		
+		Stack<Node> stack = new Stack<Node>();
+		Node current = node;
+		boolean done = false;
+		while(!done){
+			if(current != null){
+			stack.push(current);
+			current = current.getLeft();		
+			}
+			else{
+				if(!stack.empty()){
+					current = stack.pop();
+					System.out.print(current.getData() + " ");
+					current = current.getRight();
+				}
+				else
+					done = true;
+			}
+		}
+	}
+	public void postorderTraversalIterative(Node node){
+		if(node == null) return;
+		Stack<Node> stack = new Stack<Node>();
+		stack.push(node);
+		Node prev = null;
+		while(!stack.empty()){
+			Node current = stack.peek();
+			if(prev ==null || prev.getLeft()==current || prev.getRight()==current){//we are traversing down the tree
+				if(current.getLeft()!= null){
+					stack.push(current.getLeft());
+				}
+				else if(current.getRight()!=null){
+					stack.push(current.getRight());
+				}
+				else{
+					System.out.print(current.getData()+ " ");
+					stack.pop();
+				}
+			}else if(current.getLeft() == prev){
+				if(current.getRight()!=null){
+					stack.push(current.getRight());
+				}else{
+					System.out.print(current.getData()+ " ");
+					stack.pop();
+				}
+			}else if (current.getRight()== prev){
+				System.out.print(current.getData()+ " ");
+				stack.pop();
+			}
+		prev = current;	
+		}
+	}
 	
+	public static void main(String[] args){
+		BST BST = new BST();
+
+		//int inputArray[] = new int[10];
+		// inputArray[0] = 26;
+		// for(int i = 1; i < 10; i++){
+		// inputArray[i] = i*i;
+		// }
+		// for(int i = 0; i < 10; i++){
+		// RBT.insert(inputArray[i]);
+		// }
+		BST.insert(8);
+		BST.insert(4);
+		BST.insert(2);
+		BST.insert(1);
+		BST.insert(3);
+		BST.insert(6);
+		BST.insert(5);
+		BST.insert(7);
+		BST.insert(12);
+		BST.insert(10);
+		BST.insert(9);
+		BST.insert(11);
+		BST.insert(14);
+		BST.insert(13);
+		BST.insert(15);
+		
+		BST.preorderTraversal();
+		System.out.println(" ");
+		
+		BST.delete(3);
+		BST.deleteNorecursion(10);
+		
+		System.out.println("PREORDER WALK");
+		BST.preorderTraversal();
+		System.out.println(" ");
+		BST.preorderTraversalIterative(BST.getRoot());
+		System.out.println(" ");
+		
+		System.out.println("INORDER WALK");
+		BST.inorderTraversal();
+		System.out.println(" ");
+		BST.inorderTraversalIterative(BST.getRoot());
+		System.out.println(" ");
+		
+		System.out.println("POST ORDER WALK");
+		BST.postorderTraversal();
+		System.out.println(" ");
+		BST.postorderTraversalIterative(BST.getRoot());
+		System.out.println(" ");
+
+
+		System.out.println("Depth is: " + BST.maxDepth());
+		System.out.println(BST.search(19));
+	}
 }
