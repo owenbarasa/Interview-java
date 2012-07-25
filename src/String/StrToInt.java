5e4462928391bd8170379071dafaa1831e50a824
Wenqi Zhu <7.cheer@gmail.com>
2012-07-24 17:33:15 -0700

    1st

--- src/String/StrToInt.java
+++ src/String/StrToInt.java
@@ -10,5 +10,36 @@ package String;
  * Cracking the Coding Interview 
  */
 public class StrToInt {
+	public int strToInt(String str){
+		int num = 0;
+		int i = 0;
+		int len = str.length();
+		boolean isNeg = false;
+	
+		if(str.charAt(0)=='-'){
+			isNeg = true;
+			i = 1;
+		}
+		
+		while(i<len){
+			num *= 10;
+			num += (str.charAt(i) - '0');
+			i++;
+		}
+		
+		if(isNeg){
+			num = num*(-1);
+		}
+		return num;
+	}
+	
+	
+	public static void main(String[] args){
+		StrToInt result = new StrToInt();
+		String str ="-4910005";
+		int num = result.strToInt(str);
+		System.out.print(num);
+		
+	}
 
 }
