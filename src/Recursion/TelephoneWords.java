b474d02834c9f11fb4410ba7c09a9811e9b620c1
Wenqi Zhu <7.cheer@gmail.com>
2012-07-31 17:20:03 -0700

    add no recursion solution

--- src/Recursion/TelephoneWords.java
+++ src/Recursion/TelephoneWords.java
@@ -13,7 +13,7 @@ package Recursion;
 public class TelephoneWords {
 
 	public char getCharKey(int telephoneKey, int place){
-		char letters[][] ={{'0','0'},{'0','1'},{'0','A','B','C'},{'0','D','E','F'},{'0','G','H','I'},{'0','J','K','L'},{'0','M','N','O'},{'0','P','R','S'},{'0','T','U','V'},{'0','W','X','Y'}};
+		char letters[][] ={{'0','0','0','0'},{'0','1','1','1'},{'0','A','B','C'},{'0','D','E','F'},{'0','G','H','I'},{'0','J','K','L'},{'0','M','N','O'},{'0','P','R','S'},{'0','T','U','V'},{'0','W','X','Y'}};
 		return letters[telephoneKey][place];
 	}
 	
@@ -36,12 +36,44 @@ public class TelephoneWords {
 		}
 	}
 	
+	public void printTelephoneWordsNoRecursion(int phoneNum[]){
+		char[] result = new char[PHONE_NUMBER_LENGTH];
+		int i;
+		for(i=0;i<PHONE_NUMBER_LENGTH;i++){//Initialize the result
+		result[i]=getCharKey(phoneNum[i],1);
+		}
+		
+		while(true){//main loop begins
+			for(i=0;i<PHONE_NUMBER_LENGTH;++i){
+				System.out.print(result[i]);
+			}
+			System.out.print('\n');
+			
+			for(i=PHONE_NUMBER_LENGTH-1;i>=-1;i--){//Start at the end and try to increment from right to left.
+				if(i == -1) return; //you are done because you tried to carry the leftmost digit.
+				
+				if(getCharKey(phoneNum[i],3) == result[i]||phoneNum[i] ==0 ||phoneNum[i] ==1){
+					result[i] = getCharKey(phoneNum[i],1);// No break, continues to next digit
+				}
+				else if(getCharKey(phoneNum[i],1)== result[i]){
+					result[i] = getCharKey(phoneNum[i],2);
+					break;
+				}
+				else if(getCharKey(phoneNum[i],2)== result[i]){
+					result[i] = getCharKey(phoneNum[i],3);
+					break;
+				}
+			}
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TelephoneWords result = new TelephoneWords();
 		int[] phoneNum = {4,9,7,1,9,2,7};
-		result.printTelephoneWords(phoneNum);
+		//result.printTelephoneWords(phoneNum);
+		result.printTelephoneWordsNoRecursion(phoneNum);
 	}
 
 }
