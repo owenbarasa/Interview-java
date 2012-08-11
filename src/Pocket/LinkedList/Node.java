c49e5ccf86850e6e0489528584a4e80521a55e95
Wenqi Zhu <7.cheer@gmail.com>
2012-08-11 00:58:11 -0700

    Node

---
+++ src/Pocket/LinkedList/Node.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package Pocket.LinkedList;
+
+/**
+ * Created by Wenqi Zhu
+ * 1:20:00 PM Aug 10, 2012
+ * @SanFrancisco
+ * Pocket questions
+ */
+public class Node {
+	
+	Node next;
+	Object val;
+	
+	public Node(Object o){
+		val = o;
+		next = null;
+	}
+
+}
