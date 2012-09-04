14f57de5229549be0747cd40b8b3b222ef38db7e
Wenqi Zhu <7.cheer@gmail.com>
2012-09-03 20:51:29 -0700

    Longest common prefix

---
+++ src/LeetCode/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package LeetCode;
+
+/**
+ * Created by Wenqi Zhu
+ * 8:37:26 PM Sep 3, 2012
+ * @SanFrancisco
+ * LeetCode Problems
+ */
+public class LongestCommonPrefix {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
