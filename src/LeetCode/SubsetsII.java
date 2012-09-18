b894161b83cb22327f19ddfa9649c15d67c52061
Wenqi Zhu <7.cheer@gmail.com>
2012-09-18 16:58:12 -0700

    updated subsets&subsetsII

--- src/LeetCode/SubsetsII.java
+++ src/LeetCode/SubsetsII.java
@@ -2,33 +2,34 @@
  * 
  */
 package LeetCode;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by Wenqi Zhu
- * 12:36:08 PM Sep 18, 2012
+ * 4:54:20 PM Sep 18, 2012
  * @SanFrancisco
  * LeetCode Problems
- * Given a set of distinct integers, S, return all possible subsets.
+ * Given a collection of integers that might contain duplicates, S, return all possible subsets.
 
-	Note:
-	Elements in a subset must be in non-descending order.
-	The solution set must not contain duplicate subsets.
-	For example,
-	If S = [1,2,3], a solution is:
-	
-	[
-	  [3],
-	  [1],
-	  [2],
-	  [1,2,3],
-	  [1,3],
-	  [2,3],
-	  [1,2],
-	  []
-	]
+Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If S = [1,2,2], a solution is:
+
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
  */
-public class Subsets {
-	public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+public class SubsetsII {
+	public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] S) {
         // Start typing your Java solution below
         // DO NOT write main() function
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
@@ -41,11 +42,13 @@ public class Subsets {
     }
     
 	public void getSubsets(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> subset,int len,int level,int start,int[] S){
-		for(int i = start;i<len;i++){		
+		for(int i = start;i<len;i++){
+			
 			subset.add(S[i]);
+			if(!result.contains(subset)){
 			ArrayList<Integer> copy = new ArrayList<Integer>(subset);
 			result.add(copy);
-	
+			}
 			if(i<len-1){
 				getSubsets(result,subset,len,level+1,i+1,S);
 			}
