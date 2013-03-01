d7a843c811d8d2319a32c018aefcd5ce78d2617c
Wenqi Zhu <7.cheer@gmail.com>
2013-02-28 22:17:06 -0500

    1st

--- src/LeetCode2/ThreeSumClosest.java
+++ src/LeetCode2/ThreeSumClosest.java
@@ -1,12 +1,13 @@
 package LeetCode2;
-import java.util.*;
+
+import java.util.Arrays;
+
 /**
  * Created by Wenqi Zhu
- * 11:23:53 PM Nov 17, 2012
+ * 9:56:18 PM Feb 28, 2013
  * @Buffalo
  * LeetCode Problems
- * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. 
- * You may assume that each input would have exactly one solution.
+ * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
 
     For example, given array S = {-1 2 1 -4}, and target = 1.
 
@@ -16,28 +17,31 @@ public class ThreeSumClosest {
 	public int threeSumClosest(int[] num, int target) {
         // Start typing your Java solution below
         // DO NOT write main() function
-       Arrays.sort(num);
-       int i = 0;
-       int closestSum = num[0] + num[1] + num[2];
-       
-       for(i = 0;i<num.length-2;i++){
-    	   int j = i+1;
-    	   int k = num.length-1;
-    	   while(j<k){
-    		   int sum = num[i]+num[j]+num[k];
-    		   if(Math.abs(sum-target)<Math.abs(closestSum-target)){
-    			   closestSum = sum;
-    		   }
-    		   if(sum == target){
-    			   return sum;
-    		   }
-    		   else if(sum<target){
-    			   j++;
-    		   }
-    		   else
-    			   k--;   
-    	   }
-       }
-       return closestSum;
+        Arrays.sort(num);
+        int tmp = num[0] + num[1] +num[2];
+        int i;
+        
+        for(i = 0; i<num.length-2; i++){
+        	int j = i+1;
+        	int k = num.length-1;
+        	while(j<k){
+        		int sum = num[i]+num[j]+num[k];
+        		if(Math.abs(sum-target) < Math.abs(tmp-target)){
+        			tmp = sum;
+        		}
+        		
+        		if(sum == target){
+        			return sum;
+        		}
+        		else if(sum<target){
+        			j++;
+        		}
+        		else{
+        			k--;
+        		}
+        			
+        	}
+        }
+        return tmp;
     }
 }
